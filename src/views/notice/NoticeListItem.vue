222b4f2a8afaca82f127fc6ffb74d8a21f9966ff
kjg_0701@naver.com <kjg070146@gmail.com>
2023-05-24 22:16:05 +0900

    feat(notice) : 파일 다운로드 코드 수정

--- src/views/notice/NoticeListItem.vue
+++ src/views/notice/NoticeListItem.vue
@@ -3,12 +3,14 @@
     <div class="body-no">{{ article.noticeid }}</div>
     <div class="body-title" @click="onClickArticle">{{ article.title }}</div>
     <div class="body-writer">관리자</div>
-    <div class="body-date">{{ article.registerdate }}</div>
+    <div class="body-date">{{ elapsedTime(article.registerdate) }}</div>
     <div class="body-hit">{{ article.hit }}</div>
   </div>
 </template>
 
 <script>
+import AWS from "aws-sdk";
+
 export default {
   name: "TripNoticeListItem",
   components: {},
@@ -16,12 +18,72 @@ export default {
   data() {
     return {
       message: "",
+      fileList: [],
     };
   },
-  created() {},
+  created() {
+    this.getFiles();
+  },
   methods: {
     onClickArticle() {
-      this.$router.push(`/board/list/${this.article.noticeNo}`);
+      this.$router.push(`/board/list/${this.article["board_id"]}`);
+    },
+    elapsedTime(date) {
+      const start = new Date(date);
+      const end = new Date(); // 현재 날짜
+
+      const diff = (end - start) / 1000; // 경과 시간
+
+      const times = [
+        { name: "시간", milliSeconds: 60 * 60 },
+        { name: "분", milliSeconds: 60 },
+      ];
+
+      // 년 단위부터 알맞는 단위 찾기
+      for (const value of times) {
+        const betweenTime = Math.floor(diff / value.milliSeconds);
+
+        if (betweenTime >= 24) {
+          return date.split(" ")[0];
+        }
+        // 큰 단위는 0보다 작은 소수 단위 나옴
+        if (betweenTime > 0) {
+          return `${betweenTime}${value.name} 전`;
+        }
+      }
+      // 모든 단위가 맞지 않을 시
+      return "방금 전";
+    },
+    getFiles() {
+      AWS.config.update({
+        region: process.env.VUE_APP_BUCKET_REGION,
+        credentials: new AWS.CognitoIdentityCredentials({
+          IdentityPoolId: process.env.VUE_APP_IDENTITY_POLL_ID,
+        }),
+      });
+
+      const s3 = new AWS.S3({
+        apiVersion: "2006-03-01",
+        params: {
+          Bucket: process.env.VUE_APP_ALBUM_BUCKET_NAME,
+        },
+      });
+
+      s3.listObjects(
+        {
+          Delimiter: "/",
+        },
+        (err, data) => {
+          if (err) {
+            return;
+          } else {
+            this.fileList = data.Contents;
+          }
+        }
+      );
+    },
+    getFileName() {
+      //api로 업로드 된 file name 가져와서 fileList에 같은 것을 추려냄
     },
   },
 };
