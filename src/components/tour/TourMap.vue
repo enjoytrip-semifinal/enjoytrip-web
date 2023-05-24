f87c456c8c5d20fea3b82297c0a36266267d5cc8
ksg2388 <ksg2388@naver.com>
2023-05-24 17:42:00 +0900

    feat(map): 지도 거리 계산 및 경로 기능 구현

--- src/components/tour/TourMap.vue
+++ src/components/tour/TourMap.vue
@@ -74,23 +74,27 @@
         <div class="name">음식점</div>
       </div>
     </div>
-    <div class="tour-plan-button" @click="onClickPlanBtn()">
+    <div class="tour-plan-button" v-if="!isPlanShow" @click="onClickPlanBtn()">
       <span class="material-symbols-outlined">
         distance
       </span>
     </div>
     <div class="tour-plan-sidebar" :class="{active: isPlanShow}">
       <h3 class="plan-title">여행 계획</h3>
-      <div class="close-button" @click="onClickSidebarCloseBtn()"><span class="material-symbols-outlined">
+      <div class="close-button" @click="onClickSidebarCloseBtn(plan)"><span class="material-symbols-outlined">
 chevron_right
 </span></div>
       <div class="plans">
-        <div
+        <draggable v-model="planList" v-bind="dragOptions"
+        @start="drag = true"
+        @end="drag = false">
+          <transition-group>
+            <div
             class="plan"
             v-for="(plan, index) in planList"
             :key="index"
           >
-          <span class="material-symbols-outlined">
+          <span class="material-symbols-outlined" @click="onClickPlanClose(plan)">
             close
           </span>
             <img :src="plan.firstimage" alt="" />
@@ -99,6 +103,16 @@ chevron_right
               <p class="address">{{ plan.addr1 + ' ' + plan.addr2 }}</p>
             </div>
           </div>
+          </transition-group>
+        </draggable>
+      </div>
+      <div class="button-area">
+        <div class="reset-button" @click="onClickResetPlanBtn()"><span class="material-symbols-outlined">
+delete
+</span></div>
+        <div class="resgister-button"><span class="material-symbols-outlined">
+arrow_forward
+</span></div>
       </div>
     </div>
     <div class="map-area">
@@ -139,7 +153,7 @@ chevron_right
           </infinite-loading>
         </div>
       </div>
-      <div id="map" ref="map">
+      <div class="map" id="map" ref="map">
         <div class="overlay-popup" ref="placeOverlay" slot="overlay">
           <div v-if="overlayPlace">
             <img class="overlay-image" :src="overlayPlace.firstimage" />
@@ -163,13 +177,14 @@ import MarkerHandler from '@/utils/map/marker-handler';
 import KakaoOverlay from '@/utils/map/overlay';
 import InfiniteLoading from 'vue-infinite-loading';
 import { mapState } from 'vuex';
+import draggable from 'vuedraggable'
 
 const userStore = 'userStore';
 
 let kakao = window.kakao;
 export default {
   name: 'TripMap',
-  components: { InfiniteLoading },
+  components: { InfiniteLoading, draggable },
   data() {
     return {
       mapInstance: null,
@@ -193,6 +208,11 @@ export default {
       contentType: '',
       isPlanShow: false,
       planList: [], // 내 여행계획 리스트
+      planLine: null, // 여행 경로
+      dots: [], // 선이 그려지고 있을때 클릭할 때마다 클릭 지점과 거리를 표시하는 커스텀 오버레이 배열입니다.
+      circleOverlay: null,
+      distanceOverlay: null,
+      totalDistanceOverlay: null,
     };
   },
 
@@ -449,11 +469,214 @@ export default {
       this.isPlanShow = false
     },
     onClickPlanAddBtn(place) {
+      for (let i = 0; i < this.planList.length; i++) {
+        if ((this.planList[i].contentid + this.planList[i].contenttypeid) === (place.contentid + place.contenttypeid)) {
+          return;
+        }
+      }
       this.planList.push(place);
     },
+    onClickPlanClose(place) {
+      console.log(place);
+      this.planList = this.planList.filter((plan) => (plan.contentid + plan.contenttypeid) !== (place.contentid + place.contenttypeid))
+    },
+    onClickResetPlanBtn() {
+      this.planList = [];
+    },
+    drawLine() {
+      const linePath = this.planList.map((plan) => {
+        console.log(plan);
+        return new kakao.maps.LatLng(plan.mapy, plan.mapx);
+      });
+      console.log('[planList]', linePath);
+
+      linePath.forEach((line, index) => {
+        if (index === 0) {
+          this.planLine = new kakao.maps.Polyline({
+            map: this.mapInstance, // 선을 표시할 지도입니다 
+            path: [line], // 선을 구성하는 좌표 배열입니다 클릭한 위치를 넣어줍니다
+            strokeWeight: 3, // 선의 두께입니다 
+            strokeColor: '#db4040', // 선의 색깔입니다
+            strokeOpacity: 1, // 선의 불투명도입니다 0에서 1 사이값이며 0에 가까울수록 투명합니다
+            strokeStyle: 'solid' // 선의 스타일입니다
+          });
+          this.displayCircleDot(line, 0);
+        }
+        else {
+// 그려지고 있는 선의 좌표 배열을 얻어옵니다
+let path = this.planLine.getPath();
+
+// 좌표 배열에 클릭한 위치를 추가합니다
+path.push(line);
+
+// 다시 선에 좌표 배열을 설정하여 클릭 위치까지 선을 그리도록 설정합니다
+this.planLine.setPath(path);
+
+let distance = Math.round(this.planLine.getLength());
+this.displayCircleDot(line, distance);
+        }
+      })
+
+      // 마우스 클릭으로 그린 선의 좌표 배열을 얻어옵니다
+      var path = this.planLine.getPath();
+      // 선을 구성하는 좌표의 개수가 2개 이상이면
+      if (path.length > 1) {
+
+// 마지막 클릭 지점에 대한 거리 정보 커스텀 오버레이를 지웁니다
+if (this.dots[this.dots.length-1].distance) {
+  this.dots[this.dots.length-1].distance.setMap(null);
+  this.dots[this.dots.length-1].distance = null;    
+}
+
+var distance = Math.round(this.planLine.getLength()), // 선의 총 거리를 계산합니다
+    content = this.getTimeHTML(distance); // 커스텀오버레이에 추가될 내용입니다
+    
+// 그려진 선의 거리정보를 지도에 표시합니다
+this.showDistance(content, path[path.length-1]);  
+
+} else {
+
+// 선을 구성하는 좌표의 개수가 1개 이하이면 
+// 지도에 표시되고 있는 선과 정보들을 지도에서 제거합니다.
+this.deleteClickLine();
+this.deleteCircleDot(); 
+this.deleteDistnce();
+}
+      
+    },
+    deleteClickLine() {
+      if (this.planLine) {
+          this.planLine.setMap(null);    
+          this.planLine = null;        
+      }
+    },
+
+    // 선이 그려지고 있는 상태일 때 지도를 클릭하면 호출하여 
+// 클릭 지점에 대한 정보 (동그라미와 클릭 지점까지의 총거리)를 표출하는 함수입니다
+displayCircleDot(position, distance) {
+    // 클릭 지점을 표시할 빨간 동그라미 커스텀오버레이를 생성합니다
+    this.circleOverlay = new kakao.maps.CustomOverlay({
+        content: '<span class="dot">abc...</span>',
+        position: position,
+        zIndex: 1
+    });
+
+    // 지도에 표시합니다
+    console.log('[circleOverlay]', this.circleOverlay);
+    this.circleOverlay.setMap(this.mapInstance);
+
+    if (distance > 0) {
+        // 클릭한 지점까지의 그려진 선의 총 거리를 표시할 커스텀 오버레이를 생성합니다
+        this.distanceOverlay = new kakao.maps.CustomOverlay({
+            content: '<div class="dotOverlay">거리 <span class="number">' + distance + '</span>m</div>',
+            position: position,
+            yAnchor: 1,
+            zIndex: 2
+        });
+
+        // 지도에 표시합니다
+        this.distanceOverlay.setMap(this.mapInstance);
+    }
+
+    // 배열에 추가합니다
+    this.dots.push({circle: this.circleOverlay, distance: this.distanceOverlay});
+    },
+
+    // 클릭 지점에 대한 정보 (동그라미와 클릭 지점까지의 총거리)를 지도에서 모두 제거하는 함수입니다
+    deleteCircleDot() {
+
+    for ( let i = 0; i < this.dots.length; i++) {
+        if (this.dots[i].circle) { 
+          this.dots[i].circle.setMap(null);
+        }
+
+        if (this.dots[i].distance) {
+          this.dots[i].distance.setMap(null);
+        }
+    }
+
+    this.dots = [];
+    },
+
+    getTimeHTML(distance) {
+
+// 도보의 시속은 평균 4km/h 이고 도보의 분속은 67m/min입니다
+var walkkTime = distance / 67 | 0;
+var walkHour = '', walkMin = '';
+
+// 계산한 도보 시간이 60분 보다 크면 시간으로 표시합니다
+if (walkkTime > 60) {
+    walkHour = '<span class="number">' + Math.floor(walkkTime / 60) + '</span>시간 '
+}
+walkMin = '<span class="number">' + walkkTime % 60 + '</span>분'
+
+// 자전거의 평균 시속은 16km/h 이고 이것을 기준으로 자전거의 분속은 267m/min입니다
+var bycicleTime = distance / 227 | 0;
+var bycicleHour = '', bycicleMin = '';
+
+// 계산한 자전거 시간이 60분 보다 크면 시간으로 표출합니다
+if (bycicleTime > 60) {
+    bycicleHour = '<span class="number">' + Math.floor(bycicleTime / 60) + '</span>시간 '
+}
+bycicleMin = '<span class="number">' + bycicleTime % 60 + '</span>분'
+
+// 거리와 도보 시간, 자전거 시간을 가지고 HTML Content를 만들어 리턴합니다
+var content = '<ul class="dotOverlay distanceInfo">';
+content += '    <li>';
+content += '        <span class="label">총거리</span><span class="number">' + distance + '</span>m';
+content += '    </li>';
+content += '    <li>';
+content += '        <span class="label">도보</span>' + walkHour + walkMin;
+content += '    </li>';
+content += '    <li>';
+content += '        <span class="label">자전거</span>' + bycicleHour + bycicleMin;
+content += '    </li>';
+      content += '</ul>'
+
+return content;
+    },
+    deleteDistnce () {
+    if (this.totalDistanceOverlay) {
+      this.totalDistanceOverlay.setMap(null);
+      this.totalDistanceOverlay = null;
+    }
+    },
+
+
+    showDistance(content, position) {
+      console.log('[content]', content);
+    
+    if (this.totalDistanceOverlay) { // 커스텀오버레이가 생성된 상태이면
+      console.log('이미있음!!!');
+        // 커스텀 오버레이의 위치와 표시할 내용을 설정합니다
+        this.totalDistanceOverlay.setPosition(position);
+        this.totalDistanceOverlay.setContent(content);
+        
+    } else { // 커스텀 오버레이가 생성되지 않은 상태이면
+        console.log('생성!!!');
+        // 커스텀 오버레이를 생성하고 지도에 표시합니다
+        this.totalDistanceOverlay = new kakao.maps.CustomOverlay({
+            map: this.mapInstance, // 커스텀오버레이를 표시할 지도입니다
+            content: content,  // 커스텀오버레이에 표시할 내용입니다
+            position: position, // 커스텀오버레이를 표시할 위치입니다.
+            xAnchor: 0,
+            yAnchor: 0,
+            zIndex: 3  
+        });      
+    }
+    },
+
   },
   computed: {
     ...mapState(userStore, ['userInfo']),
+    dragOptions() {
+      return {
+        animation: 200,
+        group: "description",
+        disabled: false,
+        ghostClass: "ghost"
+      };
+    }
   },
   watch: {
     'mapOption.level'(cur, prev) {
@@ -480,15 +703,31 @@ export default {
       this.markers.removeAll(this.markerList);
       this.markerList = [];
     },
+    planList() {
+      this.deleteCircleDot();
+      this.deleteClickLine();
+      this.deleteDistnce();
+      this.drawLine();
+    },
   },
 };
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
+
+.dot {overflow:hidden;float:left;width:12px;height:12px;background: url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/mini_circle.png');}    
+.dotOverlay {position:relative;bottom:10px;border-radius:6px;border: 1px solid #ccc;border-bottom:2px solid #ddd;float:left;font-size:12px;padding:5px;background:#fff;}
+.dotOverlay:nth-of-type(n) {border:0; box-shadow:0px 1px 2px #888;}    
+.number {font-weight:bold;color:#ee6152;}
+.dotOverlay:after {content:'';position:absolute;margin-left:-6px;left:50%;bottom:-8px;width:11px;height:8px;background:url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white_small.png')}
+.distanceInfo {position:relative;top:5px;left:5px;list-style:none;margin:0;}
+.distanceInfo .label {display:inline-block;width:50px;}
+.distanceInfo:after {content:none;}
 .map-container {
   position: relative;
   flex: 1;
   overflow: hidden;
+
   .controller {
     position: absolute;
     right: 20px;
@@ -639,12 +878,28 @@ export default {
       align-items: center;
       justify-content: center;
       padding: 6px 0px;
-      top: 50%;
+      top: 45%;
       left: -24px;
       background-color: white;
       border-radius: 4px 0px 0px 4px ;
       cursor: pointer;
       box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.25);
+    }
+
+    .button-area {
+      height: 44px;
+      display: flex;
+      justify-content: flex-end;
+      margin-top: 10px;
+      margin-bottom: 4px;
+      .resgister-button {
+        margin-right: 20px;
+        cursor: pointer;
+      }
+      .reset-button {
+        margin-right: 20px;
+        cursor: pointer;
+      }
     }
 
     .plans {
@@ -660,6 +915,12 @@ export default {
         &::-webkit-scrollbar {
           display: none; /* 크롬, 사파리, 오페라, 엣지 */
         }
+      
+      span {
+        display: flex;
+      flex-direction: column;
+      gap: 12px;
+      }
 
       .plan {
         position: relative;
@@ -712,6 +973,7 @@ export default {
       height: 100%;
       flex: 1 1 auto;
     }
+
     .sidebar-container {
       position: relative;
       display: flex;
