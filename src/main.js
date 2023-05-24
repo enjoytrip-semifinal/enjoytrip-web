a8d0ec286b2c56ab0cf29b3a60f6e2c8cc593b76
ksg2388 <ksg2388@naver.com>
2023-05-24 10:59:46 +0900

    feat(map): map검색 기능 및 category tag 추가

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import '@/assets/css/reset.css';
+import InfiniteLoading from 'vue-infinite-loading';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  InfiniteLoading,
   render: (h) => h(App),
 }).$mount('#app');
